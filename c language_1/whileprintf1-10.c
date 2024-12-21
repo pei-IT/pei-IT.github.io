9b1b5ff42bd4bb10dd4e5e104e390a3622f678af
patrick <pei2092@gmail.com>
2024-12-21 23:53:13 +0800

    Delete c language_1 directory

--- c language_1/whileprintf1-10.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int count=1;
-    while(count<=10){ //最後一次count=11>10跳出迴圈
-        printf("%d\n",count);
-        count=count+1; //程式中同樣的規則重複做=迴圈
-    }
-    return 0;
-}
--- c language_1/whileprintf1-n.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int count=10;
-    while(count<=20){
-        printf("%d\n",count);
-        count=count+1;
-    }
-    return 0;
-}
